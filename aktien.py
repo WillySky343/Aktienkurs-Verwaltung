607166e39d21c7c42bd08afe7fcae7e32a43c95e
Willy Zhao <if25b309@technikum-wien.at>
2026-03-14 15:18:58 +0100

    fixed some errors

--- aktien.py
+++ aktien.py
@@ -24,29 +24,36 @@ class stockmanager:
         else:
             print("Aktie nicht gefunden.")
 
-    
+def main():
+    manager = stockmanager()
 
-while True:
-    print("----- Aktienverwaltung -----")
-    print("1.ADD 2.DEL 3.IMPORT 4.SEARCH 5.PLOT 6.SAVE 7.LOAD 8.QUIT")
-    choice = input("Auswahl: ")
+    while True:
+        print("----- Aktienverwaltung -----")
+        print("1.ADD 2.DEL 3.IMPORT 4.SEARCH 5.PLOT 6.SAVE 7.LOAD 8.QUIT")
+        choice = input("Auswahl: ")
 
-    if choice == '1':
-        name = input("Name: ")
-        wkn = input("WKN: ")
-        kuerzel = input("Kürzel: ")
+        if choice == '1':
+            manager.add_stock(
+                input("Name: "),
+                input("WKN: "),
+                input("Kürzel: ")
+            )
+        elif choice == '2':
+            manager.delete_stock(
+                input("Name oder Kürzel zum Löschen: ")
+            )
+        elif choice == '3':
+            print("placeholder for import functionality")
+        elif choice == '4':
+            search_input = input("Suche nach Name oder Kürzel: ")
+        elif choice == '5':
+            plot_input = input("Plot für Aktie: ")
+        elif choice == '6':
+            print("placeholder for save functionality")
+        elif choice == '7':
+            print("placeholder for load functionality")
+        elif choice == '8':
+            break
     
-    elif choice == '2':
-        del_input = input("Name oder Kürzel zum Löschen: ")
-    elif choice == '3':
-        print("placeholder for import functionality")
-    elif choice == '4':
-        search_input = input("Suche nach Name oder Kürzel: ")
-    elif choice == '5':
-        plot_input = input("Plot für Aktie: ")
-    elif choice == '6':
-        print("placeholder for save functionality")
-    elif choice == '7':
-        print("placeholder for load functionality")
-    elif choice == '8':
-        break
+if __name__ == "__main__":
+    main()
